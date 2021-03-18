e39e1e14ad099dd36cd1dbc1c556af0e4d6a80de
Ouma Joel <60340832+Godcode100@users.noreply.github.com>
2021-03-18 12:51:24 +0300

    Create test.py
    a file with tests for Django_blog.

---
+++ test.py
@@ -0,0 +1,46 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from .models import Post
+
+# Tests go below.
+
+class PostTests(TestCase):
+
+    def setUp(self):
+        '''This is a way to set up our test database that replicates our real database and it is destroyed
+           after we run python manage.py test in CLI.'''
+        self.user = get_user_model().objects.create_user(
+            username ='pen-tester',
+            email = 'pen-tester@gmail.com',
+            password = 'secret'
+        )
+        self.post = Post.objects.create(
+            title = 'CHAMPIONSHIP FIGHT',
+            slug = 'CHAMPIONSHIP-FIGHT',
+            content = 'Tyson Fury has agreed to take on Anthony Joshua',
+            author = self.user,
+        )
+    
+    def test_my_string_rep(self):
+        '''here we are testing the string representation function for our Post in models.py '''
+        writing = Post(title = '2')
+        self.assertEqual(str(writing), writing.title)
+
+    def test_my_post_model(self):
+        '''Here we test our fields in the test database which replicates our real database'''
+        self.assertEqual(f'{self.post.slug}','CHAMPIONSHIP-FIGHT')
+        self.assertEqual(f'{self.post.title}','CHAMPIONSHIP FIGHT')
+        self.assertEqual(f'{self.post.content}','Tyson Fury has agreed to take on Anthony Joshua')
+        self.assertEqual(f'{self.post.author}','pen-tester')
+        self.assertEqual(f'{self.post.id}','1')
+
+    def test_my_post_list_view(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code,200) 
+        self.assertTrue(response,'Tyson Fury has agreed to take on Anthony Joshua')
+        self.assertContains(response,'1')
+        self.assertTemplateUsed(response,'index.html')
+        
+     
